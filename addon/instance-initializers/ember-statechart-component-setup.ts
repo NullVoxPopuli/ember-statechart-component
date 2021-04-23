e3be5732ba404983e220526248edf0043cc818d7
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-04-23 14:20:55 -0400

    feat: new default template
    The default template for all createMachine components is now
    `{{yield this.state this.send}}` to remove common boilerplate

--- addon/instance-initializers/ember-statechart-component-setup.ts
+++ addon/instance-initializers/ember-statechart-component-setup.ts
@@ -1,12 +1,16 @@
-import { setComponentManager } from '@ember/component';
+import { setComponentManager, setComponentTemplate } from '@ember/component';
+import { hbs } from 'ember-cli-htmlbars';
 
 import ComponentManager from 'ember-statechart-component/-private/statechart-manager';
 import { StateNode } from 'xstate';
 
 // Managers are managed globally, and not per app instance
 setComponentManager((owner) => ComponentManager.create(owner), StateNode.prototype);
+setComponentTemplate(hbs`{{yield this.state this.send}}`, StateNode.prototype);
 
-export function initialize(): void {}
+export function initialize(): void {
+  /* intentionally empty */
+}
 
 export default {
   initialize,
