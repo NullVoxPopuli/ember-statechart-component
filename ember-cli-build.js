5cd345e973f0a0fd5b5f0a0ae9e03a11c1c08eb5
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-12-05 11:53:13 -0500

    chore(test-app): remove babel plugin

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,18 +3,7 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
-  let app = new EmberAddon(defaults, {
-    // 'ember-cli-babel': {
-    //   compileModules: false,
-    // },
-    babel: {
-      loose: true,
-      plugins: [
-        // ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ],
-    },
-  });
+  let app = new EmberAddon(defaults, {});
 
   /*
     This build file specifies the options for the dummy test app of this
