2b5f8c01948035026b8dbd69ad39b117f12397a6
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-21 07:34:21 -0500

    chore(internal): make toggle demo

---
+++ testing/ember-app/app/components/toggle-machine.js
@@ -0,0 +1,9 @@
+import { createMachine } from 'xstate';
+
+export default createMachine({
+  initial: 'inactive',
+  states: {
+    inactive: { on: { TOGGLE: 'active' } },
+    active: { on: { TOGGLE: 'inactive' } },
+  },
+});
