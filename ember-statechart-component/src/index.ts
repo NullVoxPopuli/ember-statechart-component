b497a76f2066b1561b53169e47dd694439c8536a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-18 09:27:49 -0400

    Expose wrapper type

--- ember-statechart-component/src/index.ts
+++ ember-statechart-component/src/index.ts
@@ -4,6 +4,7 @@ import { assert } from '@ember/debug';
 import { getOwner } from '@ember/owner';
 
 import type { Registry } from '@ember/service';
+import type { Actor, AnyActorLogic, SnapshotFrom } from 'xstate';
 
 export { UPDATE_EVENT_NAME } from './-private/statechart-manager.js';
 
@@ -18,4 +19,14 @@ export function getService<Key extends keyof Registry>(
   let service = owner.lookup(`service:${serviceName}`) as Registry[Key];
 
   return service;
+}
+
+export interface ReactiveActor<Logic extends AnyActorLogic> {
+  actor: Actor<Logic>;
+  snapshot: SnapshotFrom<Logic>;
+  value: SnapshotFrom<Logic>['value'];
+  statePath: string;
+  matches: SnapshotFrom<Logic>['matches'];
+  send: (eventType: string) => void;
+  onTransition: (callback: () => void) => void;
 }
